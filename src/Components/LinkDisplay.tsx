2702d562e7eeaedde4805fdd712e962c8df42b11
omer_sm <smorodinskyomer@gmail.com>
2024-05-04 11:50:35 +0300

    fixed links??

--- src/Components/LinkDisplay.tsx
+++ src/Components/LinkDisplay.tsx
@@ -3,13 +3,13 @@ import Typography from "@mui/joy/Typography"
 import Link from "@mui/joy/Link"
 
 const makeLink = (data: string) => {
-    return "/?g=" + btoa(data)
+    return "/tic-tac-toe/?g=" + btoa(data)
 }
 
 export default function LinkDisplay({data} : {data: string}) {
     return (
         <Typography level="body-lg" textAlign="center"><b>Your link is: </b>
-        <Link href={makeLink(data)}>omer-sm.github.io/tic-tac-toe{makeLink(data)}</Link>
+        <Link href={makeLink(data)}>omer-sm.github.io{makeLink(data)}</Link>
         <br/>Share it with a friend!</Typography>
     )
 }
